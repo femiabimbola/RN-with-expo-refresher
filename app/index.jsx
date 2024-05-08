88a20e282124106c9bc6ac1bc292e9fa87637a95
Femi Abimbola <femiabimbola6@gmail.com>
2024-05-07 22:53:15 -0700

    add the use fonts

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl">Welcome, Aora</Text>
+      <Text className="text-4 xl">Welcome, Aora</Text>
       <StatusBar style="auto" />
       <Link href='/profile' style={{color:'blue'}}>Go to Profile</Link>
     </View>
