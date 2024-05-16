12449bda1f3a2ae4a6cdb724f9d562151e7d41bb
Femi Abimbola <femiabimbola6@gmail.com>
2024-05-16 13:57:50 -0700

    complete the setup

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Tabs, Redirect } from "expo-router";
 
 const TabLayout = () => {
   return (
-    <View>
-      <Text> layout</Text>
-    </View>
+   <>
+   <Tabs>
+    <Tabs.Screen />
+   </Tabs>
+   </>
   )
 }
 
