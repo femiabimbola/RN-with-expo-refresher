3b0cd6095724b1622987e1352d10d8e251a02173
Femi Abimbola <femiabimbola6@gmail.com>
2024-05-23 22:00:47 -0700

    updated the tab icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,15 +1,48 @@
-import { View, Text } from "react-native";
+import {View, Text, Image} from "react-native";
 import React from "react";
-import { Tabs, Redirect } from "expo-router";
+import {Tabs, Redirect} from "expo-router";
+import {icons} from "../../constants";
+
+const TabIcon = ({icon, color, name, focused}) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabLayout = () => {
   return (
-   <>
-   <Tabs>
-    <Tabs.Screen />
-   </Tabs>
-   </>
-  )
-}
+    <>
+      <Tabs>
+        <Tabs.Screen
+          name="home"
+          options={{
+            headerShown: false,
+            title: "Home",
+            tabBarIcon: ({color, focused}) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
 
-export default TabLayout
+export default TabLayout;
