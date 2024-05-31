ce6f038618afc1b5946aecfe8bbd1bb696a0853b
Femi Abimbola <femiabimbola6@gmail.com>
2024-05-30 22:46:55 -0700

    working on the sign up

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,28 @@
-import { View, Text } from "react-native";
+import {View, Text} from "react-native";
 import React from "react";
+import {Stack} from "expo-router";
+import {StatusBar} from "expo-status-bar";
 
+//  Essence of the stack screen
 const AuthLayout = () => {
   return (
-    <View>
-      <Text> layout</Text>
-    </View>
-  )
-}
+    <>
+      <Stack>
+        <Stack.Screen
+          name="signin"
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{headerShown: false}}
+        />
+      </Stack>
+      <StatusBar
+        backgroundColor={"#161622"}
+        style="light"
+      />
+    </>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
---
+++ components/FormField.jsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+const FormField = () => {
+  return (
+    <View className="space-x-2">
+      <Text className="text-base text-gray-100 font-pmedium"> Hi</Text>
+    </View>
+  );
+};
+
+export default FormField;
