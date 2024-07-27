42e6f4d9cc422e3b1492a66e5f4cdba86e2839d2
femi abimbola <femiabimbola6@gmail.com>
2024-07-27 02:23:30 -0700

    creating search input

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,38 @@
-import { View, Text } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { images } from "../../constants";
+
 const Home = () => {
   return (
-    <SafeAreaView>
-   
-      <Text> Home </Text>
+    <SafeAreaView className="bg-primary">
+      <FlatList 
+        data={[{id: 1, }, {id: 2, }, {id: 3, }]} 
+        keyExtractor={(item) => {item.$id}}
+        ListHeaderComponent={ () => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100"> Welcome Back</Text>
+                <Text className="text-2xl font-psemibold text-white"> Femi Abimbola</Text>
+              </View>
+              <View className="mt-1.5"> 
+                <Image 
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+              
+            </View>
+          </View>
+        )}
+        renderItem={({item}) => (
+        <Text className="text-3xl text-white"> {item.id}</Text> 
+      )}
+
+      />
     
     </SafeAreaView>
   )
