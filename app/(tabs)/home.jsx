c177cdc2fee25a52e99404400324bf407fc6e93d
femi abimbola <femiabimbola6@gmail.com>
2024-07-28 12:29:23 -0700

    updating the home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,5 @@
-import { FlatList, Image, Text, View } from "react-native";
-import React from "react";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../../constants";
@@ -8,20 +8,36 @@ import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // call new vide
+    setRefreshing(false)
+  };
+
   return (
-    <SafeAreaView className="bg-primary">
-      <FlatList 
-        data={[{id: 1, }, {id: 2, }, {id: 3, }]} 
-        keyExtractor={(item) => {item.$id}}
-        ListHeaderComponent={ () => (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => {
+          item.$id;
+        }}
+        ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
               <View>
-                <Text className="font-pmedium text-sm text-gray-100"> Welcome Back</Text>
-                <Text className="text-2xl font-psemibold text-white"> Femi Abimbola</Text>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  {" "}
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  {" "}
+                  Femi Abimbola
+                </Text>
               </View>
-              <View className="mt-1.5"> 
-                <Image 
+              <View className="mt-1.5">
+                <Image
                   source={images.logoSmall}
                   className="w-9 h-10"
                   resizeMode="contain"
@@ -30,21 +46,24 @@ const Home = () => {
             </View>
             <SearchInput />
             <View className="w-full flex-1 pt-3 pb-8">
-              <Text className="text-gray-100 text-lg font-pregular mb-3"> Lastest Videos</Text>
-              <Trending posts ={[{id: 1}, { id: 2}] ?? []}/>
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                {" "}
+                Lastest Videos
+              </Text>
+              <Trending posts={[{ id: 1 }, { id: 2 }] ?? []} />
             </View>
           </View>
         )}
-        renderItem={({item}) => (
-        <Text className="text-3xl text-white"> {item.id}</Text> 
-      )}
-      ListEmptyComponent={() => 
-        <EmptyState />
-      }
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white"> {item.id}</Text>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState title="No videos found" subtitle=" Create a video" />
+        )}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
       />
-    
-    </SafeAreaView>
-  )
-}
+    </SafeAreaView> 
+  );
+};
 
-export default Home
+export default Home;
