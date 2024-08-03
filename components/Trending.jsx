9dbd1c16aaf7f658575193b425a17d4818e8b32f
femi abimbola <femiabimbola6@gmail.com>
2024-08-03 06:08:35 -0700

    trending vid

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,26 +1,73 @@
-import { useState } from 'react'
-import { View, Text, FlatList } from 'react-native' 
-import * as Animatable from 'react-native-animatable'
+import { useState } from "react";
+import {
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import { icons } from "../constants";
 
-const TrendingItem = ({activeItem, item}) => {
+const zoomIn = {
+  0: { scale: 0.9 },
+  1: { scale: 1.1 },
+};
+
+const zoomOut = {
+  0: { scale: 1 },
+  1: { scale: 0.9 },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+
   return (
-    <Animatable.View classNmae="mr-5">
-
-    </Animatable.View>
-  )
-}
-
-const Trending = ({posts}) => {
-  const[activeItem, setActiveItem] = useState()
-  return (
-    <FlatList 
-      data={posts}
-      keyExtractor={(item) => (
-        <TrendingItem activeItem={activeItem} />
+    <Animatable.View
+      className="mr-3"
+      animation={activeItem.$id === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Text className="text-white"> Playing</Text>
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+          <Image source={icons.play}  className="w-12 h-12 absolute" resizeMode="contain"/>
+        </TouchableOpacity>
       )}
+    </Animatable.View>
+  );
+};
+
+const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
+
+  //an handler
+  const viewableItemsChanged = ({viewableItems}) => {
+    if(viewableItems.length > 0) setActiveItem(viewableItems[0].key)
+  }
+  return (
+    <FlatList
+      data={posts}
+      keyExtractor={(item) => item.$id}
+      renderItem={({ item }) => (
+        <TrendingItem activeItem={activeItem} item={item} />
+      )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{itemVisiblePercentThreshold : 70}}
+      contentOffset={{ x: 170}}
       horizontal
     />
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
