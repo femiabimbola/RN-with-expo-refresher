23fbabae366082843644d3cb29150c5121ecf250
femi abimbola <femiabimbola6@gmail.com>
2024-08-05 11:25:09 -0700

    working with the search page

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useAppwrite from "../../lib/useAppwrite";
@@ -7,49 +7,38 @@ import { searchPosts } from "../../lib/appWrite";
 
 const Search = () => {
   const { query } = useLocalSearchParams();
-  const {data:posts, refetch} = useAppwrite(searchPosts(query))
+  // const { data: posts, refetch } = useAppwrite(searchPosts(query));
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
   return (
-<SafeAreaView className="bg-primary h-full">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
         data={posts}
         keyExtractor={(item) => item.$id}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
-              <View>
-                <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
-                </Text>
-                <Text className="text-2xl font-psemibold text-white">
-                  Femi Abimbola
-                </Text>
-              </View>
-              <View className="mt-1.5">
-                <Image
-                  source={images.logoSmall}
-                  className="w-9 h-10"
-                  resizeMode="contain"
-                />
-              </View>
-            </View>
-            <SearchInput />
-            <View className="w-full flex-1 pt-3 pb-8">
-              <Text className="text-gray-100 text-lg font-pregular mb-3">
-                Lastest Videos
+          <View className="my-6 px-4">
+            <View>
+              <Text className="font-pmedium text-sm text-gray-100">
+                Search Results
               </Text>
-              <Trending posts={latestPosts} />
+              <Text className="text-2xl font-psemibold text-white">
+                {query}
+              </Text>
+              <View className={"mt-6 mb-8"}>
+                <SearchInput initialQuery={query} />
+              </View>
             </View>
           </View>
         )}
-        renderItem={({ item }) => (
-          <VideoCard video={item}/>
-        )}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListEmptyComponent={() => (
-          <EmptyState title="No videos found" subtitle=" Create a video" />
+          <EmptyState title="No videos found" subtitle="No Video found for this search query" />
         )}
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
       />
-    </SafeAreaView> 
+    </SafeAreaView>
   );
 };
 
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -10,17 +10,10 @@ import React, { useState } from "react";
 import { icons } from "../constants";
 import { router, usePathname } from "expo-router";
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
+const SearchInput = ({initialQuery}) => {
   const pathname = usePathname();
-  const [query, setQuery] = useState("");
-
+  const [query, setQuery] = useState(initialQuery || "");
+s
   return (
     <View className="border-2 border-black-200 w-full h-12 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
       <TextInput
