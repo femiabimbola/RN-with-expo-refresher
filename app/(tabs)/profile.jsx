a2606edd03950c697baf1f6cbf64d49b2e781a2d
femi abimbola <femiabimbola6@gmail.com>
2024-08-06 15:13:29 -0700

    working on the header component

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -2,18 +2,18 @@ import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useAppwrite from "../../lib/useAppwrite";
-import { getUserPosts,  } from "../../lib/appWrite";
+import { getUserPosts } from "../../lib/appWrite";
 import SearchInput from "../../components/SearchInput";
 import VideoCard from "../../components/VideoCard";
-import EmptyState from "../../components/EmptyState"
+import EmptyState from "../../components/EmptyState";
 import { useGlobalContext } from "../../context/GlobalProvider";
 import { icons } from "../../constants";
 
-
 //  We are using global context
 const Profile = () => {
-  const { user, setUser, setIsLoggedIn} = useGlobalContext()
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(() => getUserPosts(user.$id));
+  const logout = () => {};
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -21,15 +21,28 @@ const Profile = () => {
         data={posts}
         keyExtractor={(item) => item.$id}
         ListHeaderComponent={() => (
-          <View className="w-full justify-center items-center mt-6 mb-12 px-4" >
-            <TouchableOpacity>
-              <Image  source={icons.logout } resizeMode="contain" className="w-6 h-6"/>
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
             </TouchableOpacity>
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image  source={{ uri: user?.avatar}}/>
+            </View>
           </View>
         )}
         renderItem={({ item }) => <VideoCard video={item} />}
         ListEmptyComponent={() => (
-          <EmptyState title="No videos found" subtitle="No Video found for this search query" />
+          <EmptyState
+            title="No videos found"
+            subtitle="No Video found for this search query"
+          />
         )}
       />
     </SafeAreaView>
