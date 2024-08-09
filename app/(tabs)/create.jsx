1f57258309773da95dc47f90d2196c3809df6f3a
femi abimbola <femiabimbola6@gmail.com>
2024-08-08 23:02:10 -0700

    adding the appwrite login for dropping video

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,9 +1,12 @@
-import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Image, Alert } from "react-native";
 import { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import FormField from "../../components/FormField";
 import { Video, ResizeMode } from "expo-av";
+import * as DocumentPicker from "expo-document-picker";
 import { icons } from "../../constants";
+import CustomButton from "../../components/CustomButton";
+import { router } from "expo-router";
 
 const Create = () => {
   const [uploading, setUploading] = useState(false);
@@ -13,6 +16,51 @@ const Create = () => {
     thumbnail: null,
     prompt: "",
   });
+
+  const openPicker = async (selectType) => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type:
+        selectType === "image"
+          ? ["image/png", "image/jpg"]
+          : ["video/mp4", "video/gif"],
+    });
+
+    if(!result.canceled) {
+      if(selectType === 'image') {
+        setForm({...form, thumbnail: result.assets[0]})
+      }
+
+      if(selectType === 'video') {
+        setForm({...form, video: result.assets[0]})
+      }
+    } else {
+      setTimeout(() => {
+        Alert.alert('Document picked', JSON.stringify(result, null, 2))
+      }, 100)
+    }
+  };
+
+  const submit = () => {
+    if(!form.prompt || !form.thumbnail || !form.video || !form.title) {
+      return Alert.alert(' Fill all the fields')
+    }
+    setUploading(true)
+    try {
+      Alert.alert('Success', 'Post uploaded successfully ')
+      router.push('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setForm({
+          title: "",
+          video: null,
+          thumbnail: null,
+          prompt: "",
+        })
+      setUploading(false)
+    }
+  };
+
   return (
     <SafeAreaProvider className="bg-primary h-full">
       <ScrollView className="px-4 my-6">
@@ -26,10 +74,10 @@ const Create = () => {
         />
         <View className="mt-7 space-y-2">
           <Text className="text-base text-gray-100 font-pmedium">
-            {" "}
             Upload Video
           </Text>
-          <TouchableOpacity>
+
+          <TouchableOpacity onPress={() => openPicker("video")}>
             {form.video ? (
               <Video
                 source={{ uri: form.video.uri }}
@@ -52,8 +100,45 @@ const Create = () => {
           </TouchableOpacity>
         </View>
         <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Thumbnail Image
+          </Text>
 
+          <TouchableOpacity onPress={() => openPicker("image")}>
+            {form.thumbnail ? (
+              <Image
+                source={{ uri: form.thumbnail.uri }}
+                resizeMode="cover"
+                className="w-full h-64 rounded-2xl"
+              />
+            ) : (
+              <View className="w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2">
+                <Image
+                  source={icons.upload}
+                  resizeMode="contain"
+                  className="w-5 h-5"
+                />
+                <Text className="text-sm text-gray-100 font-pmedium">
+                  {" "}
+                  Choose a file
+                </Text>
+              </View>
+            )}
+          </TouchableOpacity>
         </View>
+        <FormField
+          title="AI Prompt"
+          value={form.prompt}
+          placeholder="The prompt used to create this video"
+          handleChangeText={(e) => setForm({ ...form, prompt: e })}
+          otherStyles={"mt-7"}
+        />
+        <CustomButton
+          title="Submit & Publish"
+          handlePress={submit}
+          containerStyle={"mt-10"}
+          isLoading={uploading}
+        />
       </ScrollView>
     </SafeAreaProvider>
   );
