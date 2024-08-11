0e0a4bbfffb7890b4c3380cd3a51b90a04c0ad44
femi abimbola <femiabimbola6@gmail.com>
2024-08-10 23:28:13 -0700

    changing the image and video picker

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -4,6 +4,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import FormField from "../../components/FormField";
 import { Video, ResizeMode } from "expo-av";
 import * as DocumentPicker from "expo-document-picker";
+import * as ImagePicker from 'expo-image-picker'
 import { icons } from "../../constants";
 import CustomButton from "../../components/CustomButton";
 import { router } from "expo-router";
@@ -21,11 +22,10 @@ const Create = () => {
   });
 
   const openPicker = async (selectType) => {
-    const result = await DocumentPicker.getDocumentAsync({
-      type:
-        selectType === "image"
-          ? ["image/png", "image/jpg"]
-          : ["video/mp4", "video/gif"],
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: selectType === 'image' ? ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
+      aspect: [4, 3],
+      quality: 1,
     });
 
     if(!result.canceled) {
@@ -36,11 +36,12 @@ const Create = () => {
       if(selectType === 'video') {
         setForm({...form, video: result.assets[0]})
       }
-    } else {
-      setTimeout(() => {
-        Alert.alert('Document picked', JSON.stringify(result, null, 2))
-      }, 100)
     }
+    //  else {
+    //   setTimeout(() => {
+    //     Alert.alert('Document picked', JSON.stringify(result, null, 2))
+    //   }, 100)
+    // }
   };
 
   const submit = async () => {
@@ -86,9 +87,7 @@ const Create = () => {
               <Video
                 source={{ uri: form.video.uri }}
                 className="w-full h-64 rounded-2xl"
-                useNativeControls
                 resizeMode={ResizeMode.COVER}
-                isLooping
               />
             ) : (
               <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
